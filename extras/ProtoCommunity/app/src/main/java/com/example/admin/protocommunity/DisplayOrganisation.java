e4960d4b73b8c967642b2f59992c2be77b116bfe
martin casey <martcasey@LIVE.IE>
2018-08-22 20:27:27 +0100

    Works as intended

--- extras/ProtoCommunity/app/src/main/java/com/example/admin/protocommunity/DisplayOrganisation.java
+++ extras/ProtoCommunity/app/src/main/java/com/example/admin/protocommunity/DisplayOrganisation.java
@@ -54,7 +54,7 @@ public class DisplayOrganisation extends AppCompatActivity {
     //Something is going on here
     private void JsonParse()
     {
-        String url = "https://api.myjson.com/bins/13o5rc";//fine
+        String url = "https://api.myjson.com/bins/1fef5c";//fine
         //our file is a JSON object, so we need this request type
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
